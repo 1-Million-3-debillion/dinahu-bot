d786a7646fe6c4576a6f96b5345f9e7d98ad3f87
Li-Khan <alihanzh40@gmail.com>
2022-11-20 23:23:17 +0600

    Add: example db

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"os"
 	"sync"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Config struct {
@@ -14,6 +18,11 @@ type Config struct {
 var (
 	cfg     Config
 	onceCfg sync.Once
+)
+
+var (
+	db     *sql.DB
+	onceDb sync.Once
 )
 
 func GetConfig() *Config {
@@ -31,9 +40,116 @@ func GetConfig() *Config {
 	return &cfg
 }
 
+func GetDb() *sql.DB {
+	onceDb.Do(func() {
+		var err error
+		db, err = sql.Open("sqlite3", "dinahu.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	return db
+}
+
+func Migration() {
+	query := `
+		CREATE TABLE IF NOT EXISTS "user" (
+			"user_id" INTEGER UNIQUE NOT NULL,
+			"first_name" TEXT,
+			"last_name" TEXT,
+			"username" TEXT
+		);
+		
+		CREATE TABLE IF NOT EXISTS "chat" (
+			"chat_id" INTEGER NOT NULL UNIQUE,
+			"name" TEXT
+		);
+
+		CREATE TABLE IF NOT EXISTS "user_chat" (
+			"id" TEXT NOT NULL UNIQUE,
+			"user_id" INTEGER NOT NULL,
+			"chat_id" INTGER NOT NULL
+		);
+		`
+
+	_, err := GetDb().Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type User struct {
+	UserID    int64
+	FirstName string
+	LastName  string
+	Username  string
+}
+
+type Chat struct {
+	ChatID int64
+	Name   string
+}
+
+type UserChat struct {
+	ID     string
+	UserID int64
+	ChatID int64
+}
+
 func init() {
 	GetConfig()
+	GetDb()
+	Migration()
 }
 
 func main() {
+	bot, err := tgbotapi.NewBotAPI(GetConfig().DinahuToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "register":
+			// TODO : зарегать юзера наху
+			msg.Text = "тут будет регистрация юзеров"
+		case "delete":
+			// TODO : удалить юзера с бд
+			msg.Text = "ди наху отсюда ты удален"
+		case "start", "run":
+			// TODO: выбрать рандомного юзера и послать его наху
+			msg.Text = "Да ди ты наху"
+		case "stats":
+			// TODO: статистику посланных наху юзеров
+			msg.Text = "тут будет статистика ди наху пон"
+		case "help":
+			msg.Text = "Ди наху со своим /help"
+		default:
+			continue
+		}
+
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/1-Million-3-debillion/dinahu-bot
 
 go 1.18
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	gopkg.in/telebot.v3 v3.1.2 // indirect
+)
